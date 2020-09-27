6e23118fb51c0ef0c91d0bf9cac8aa4948e67c66
Christian Talmo <talmo.christian@gmail.com>
2020-09-27 14:47:08 +0300

    test: add -v, --version cli args tests

--- test/index.js
+++ test/index.js
@@ -17,7 +17,53 @@ process.on( 'exit', function () {
   nz.kill()
 } )
 
-let spawn = undefined
+test( '-v, --version cli flags', function ( t ) {
+  t.plan( 6 )
+
+  const cmd = path.join( __dirname, '../bin/cli.js' )
+  const args = [ '-v' ]
+  const opts = { stdio: [ 'pipe', 'pipe', process.stderr ] }
+
+  const spawn_v = childProcess.spawn( cmd, [ '-v' ], opts )
+  const spawn_version = childProcess.spawn( cmd, [ '--version' ], opts )
+  nz.add( spawn_v.pid )
+  nz.add( spawn_version.pid )
+
+  spawn_v.stdout.on( 'data', onData1 )
+  spawn_version.stdout.on( 'data', onData2 )
+
+  spawn_v.on( 'close', finish1 )
+  spawn_version.on( 'close', finish2 )
+
+  const pkg = require(
+    path.join( __dirname, '../package.json' )
+  )
+
+  const split = pkg.version.split( '.' )
+  t.equal( split.length, 3, 'major.minor.patch found' )
+  t.ok( split[ 0 ] == 0, 'major ok' ) // will fail when first major version is set, update it then
+  t.ok( split[ 1 ] > 0, 'minor ok' )
+  t.ok( split[ 2 ] >= 0, 'patch ok' )
+
+  buffer1 = ''
+  buffer2 = ''
+
+  function onData1( chunk ) {
+    buffer1 += chunk.toString( 'utf8' )
+  }
+  function onData2( chunk ) {
+    buffer2 += chunk.toString( 'utf8' )
+  }
+
+  function finish1 () {
+    t.equal( buffer1.trim(), pkg.version, 'version OK' )
+  }
+  function finish2 () {
+    t.equal( buffer2.trim(), pkg.version, 'version OK' )
+  }
+
+} )
+
 test( 'search and play youtube video', function ( t ) {
   t.timeoutAfter( 1000 * 20 )
   const cmd = path.join( __dirname, '../bin/cli.js' )
